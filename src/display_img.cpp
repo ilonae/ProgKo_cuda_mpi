6c3113fb270998bdbc228b899303432eda42cba3
Ilona Eisenbraun <ilonaeisenbraun@gmail.com>
2020-05-11 00:02:52 +0200

    adding cuda test

--- src/display_img.cpp
+++ src/display_img.cpp
@@ -1,30 +1,14 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <iostream>
+#include "png.h"
 
-using namespace cv;
-using namespace std;
-
-int main(int argc, char **argv)
+void readpng_version_info()
 {
-    if (argc != 2)
-    {
-        cout << " Usage: display_image ImageToLoadAndDisplay" << endl;
-        return -1;
-    }
+    fprintf(stderr, "   Compiled with libpng %s; using libpng %s.\n",
+            PNG_LIBPNG_VER_STRING, png_libpng_ver);
+    fprintf(stderr, "   Compiled with zlib %s; using zlib %s.\n",
+            ZLIB_VERSION, zlib_version);
+}
 
-    Mat image;
-    image = imread(argv[1], CV_LOAD_IMAGE_COLOR); // Read the file
-
-    if (!image.data) // Check for invalid input
-    {
-        cout << "Could not open or find the image" << std::endl;
-        return -1;
-    }
-
-    namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
-    imshow("Display window", image);                // Show our image inside it.
-
-    waitKey(0); // Wait for a keystroke in the window
-    return 0;
+int main()
+{
+readpng_version_info:
 }
