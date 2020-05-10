215060001e1bf8b717b5c3f8ef0e1a09465c71a4
Ilona Eisenbraun <ilonaeisenbraun@gmail.com>
2020-05-10 17:02:40 +0200

    adding cuda test

--- test.cu
+++ test.cu
@@ -4,13 +4,37 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-__global__ void cuda_hello()
+__global__ void addVectors(const int *a, const int *b, int *c, const int &size)
 {
-    printf("Hello World from GPU!\n");
+    for (int i = 0; i < size; i++)
+    {
+        c[i] = a[i] + b[i];
+    }
 }
 
 int main()
 {
-    cuda_hello<<<1, 1>>>();
+    const int size = 1024;
+
+    int *a = new int[size];
+    int *b = new int[size];
+    int *c = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        a[i] = 1;
+        b[i] = 2;
+    }
+
+    addVectors<<<1, 1>>>(a, b, c, size);
+
+    float avg = 0;
+    for (int i = 0; i < size; i++)
+        avg += c[i];
+    avg /= size;
+    std::cout << "Average is: " << avg << ", should be: 3.0" << std::endl;
+    delete[] a;
+    delete[] b;
+    delete[] c;
     return 0;
 }
