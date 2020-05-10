357fa77690bd4a82d6b91dcff5ec3c51e23229c2
Ilona Eisenbraun <ilonaeisenbraun@gmail.com>
2020-05-10 18:06:26 +0200

    adding cuda test

--- test.cu
+++ test.cu
@@ -6,7 +6,9 @@
 
 __global__ void addVectors(const int *a, const int *b, int *c, const int &size)
 {
-    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    iint i = blockIdx.x * blockDim.x * blockDim.y +
+             blockDim.x * threadIdx.y +
+             threadIdx.x;
     c[i] = a[i] + b[i];
     printf("hello from gpu");
 };
