973cc29c094d1f9728ea938426e90cd41b392e04
Ilona Eisenbraun <ilonaeisenbraun@gmail.com>
2020-05-11 00:06:00 +0200

    adding cuda test

--- src/display_img.cpp
+++ src/display_img.cpp
@@ -2,10 +2,8 @@
 
 void readpng_version_info()
 {
-    fprintf(stderr, "   Compiled with libpng %s; using libpng %s.\n",
-            PNG_LIBPNG_VER_STRING, png_libpng_ver);
-    fprintf(stderr, "   Compiled with zlib %s; using zlib %s.\n",
-            ZLIB_VERSION, zlib_version);
+    fprintf(stderr, "   Compiled with libpng %s; using libpng\n",
+            PNG_LIBPNG_VER_STRING);
 }
 
 int main()
