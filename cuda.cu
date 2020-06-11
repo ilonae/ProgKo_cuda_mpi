565434a647da8eb5e19487056acfcd730dab8175
Ilona Eisenbraun <ilonaeisenbraun@gmail.com>
2020-06-11 17:22:47 +0200

    temp

--- cuda.cu
+++ cuda.cu
@@ -38,8 +38,8 @@ __global__ void grayscale_kernel(unsigned char* output, int width, int height, p
         const int y = blockIdx.y * blockDim.y + threadIdx.y;
         if ((x < width) && (y < height)){
             png_byte* row = row_pointers[y];
-            png_byte ptr = (row[x*3]);
-            printf("Pixel at position [ %d - %d ] has RGB values: %d - %d - %d \n", x, y, *ptr[0], *ptr[1], *ptr[2]);
+            //png_byte* ptr = &(row[x*3]);
+            //printf("Pixel at position [ %d - %d ] has RGB values: %d - %d - %d \n", x, y, ptr[0], ptr[1], ptr[2]);
             output[x] = 1;
             output[y] = 1;
             printf("hello from gpu");  
