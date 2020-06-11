c15047928e29979f131223f40e3e6c224b41cb0c
Ilona Eisenbraun <ilonaeisenbraun@gmail.com>
2020-06-11 16:27:04 +0200

    temp

--- cuda.cu
+++ cuda.cu
@@ -10,16 +10,6 @@
 #define PNG_DEBUG 3
 #include <png.h>
 
-void abort_(const char * s, ...)
-{
-        va_list args;
-        va_start(args, s);
-        vfprintf(stderr, s, args);
-        fprintf(stderr, "\n");
-        va_end(args);
-        abort();
-}
-
 int x, y;
 
 int width, height;
@@ -33,48 +23,91 @@ int number_of_passes;
 png_bytep * row_pointers;
 
 
-
-
-__global__ void grayscale_kernel(unsigned char* output, int width, int height, png_bytep* row_pointers) {
-
-        const int x = blockIdx.x * blockDim.x + threadIdx.x;
-        const int y = blockIdx.y * blockDim.y + threadIdx.y;
-       
-        
-        output[x] = 1;
-        output[y] = 1;
-        printf("hello from gpu");
-    
-
+void abort_(const char * s, ...)
+{
+        va_list args;
+        va_start(args, s);
+        vfprintf(stderr, s, args);
+        fprintf(stderr, "\n");
+        va_end(args);
+        abort();
 }
 
-
-__global__ void addVectors(const int *a, const int *b, int *c, const int &size)
-{
-    int i = blockIdx.x * blockDim.x * blockDim.y +
-            blockDim.x * threadIdx.y +
-            threadIdx.x;
-    c[i] = a[i] + b[i];
-    printf("hello from gpu");
-};
-/* 
-
-
-if ((x < width) && (y < height))
+__global__ void grayscale_kernel(unsigned char* output, int width, int height, png_bytep* row_pointers) {
+        const int x = blockIdx.x * blockDim.x + threadIdx.x;
+        const int y = blockIdx.y * blockDim.y + threadIdx.y;
+        if ((x < width) && (y < height))
         {
                 png_byte* row = row_pointers[y];
                         png_byte* ptr = &(row[x*3]);
                         printf("Pixel at position [ %d - %d ] has RGB values: %d - %d - %d \n",
                                x, y, ptr[0], ptr[1], ptr[2]);
-
-                               //ptr[0],ptr[1],ptr[2] = (ptr[0] + ptr[1] + ptr[2])/3;
-
-                
-        
+                               //ptr[0],ptr[1],ptr[2] = (ptr[0] + ptr[1] + ptr[2])/3;   
         }
+        output[x] = 1;
+        output[y] = 1;
+        printf("hello from gpu");   
+}
+
+
+void write_png_file(char* file_name){
+        /* create file */
+        FILE *fp = fopen(file_name, "wb");
+        if (!fp)
+                abort_("[write_png_file] File %s could not be opened for writing", file_name);
+
+
+        /* initialize stuff */
+        png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+
+        if (!png_ptr)
+                abort_("[write_png_file] png_create_write_struct failed");
+
+        info_ptr = png_create_info_struct(png_ptr);
+        if (!info_ptr)
+                abort_("[write_png_file] png_create_info_struct failed");
+
+        if (setjmp(png_jmpbuf(png_ptr)))
+                abort_("[write_png_file] Error during init_io");
+
+        png_init_io(png_ptr, fp);
+
+
+        /* write header */
+        if (setjmp(png_jmpbuf(png_ptr)))
+                abort_("[write_png_file] Error during writing header");
+
+        png_set_IHDR(png_ptr, info_ptr, width, height,
+                     bit_depth, color_type, PNG_INTERLACE_NONE,
+                     PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+
+        png_write_info(png_ptr, info_ptr);
+
+
+        /* write bytes */
+        if (setjmp(png_jmpbuf(png_ptr)))
+                abort_("[write_png_file] Error during writing bytes");
+
+        png_write_image(png_ptr, row_pointers);
+
+
+        /* end write */
+        if (setjmp(png_jmpbuf(png_ptr)))
+                abort_("[write_png_file] Error during end of write");
+
+        png_write_end(png_ptr, NULL);
+
+        /* cleanup heap allocation */
+        for (y=0; y<height; y++)
+                free(row_pointers[y]);
+        free(row_pointers);
+
+        fclose(fp);
+}
 
 
 
+/* 
 
 __global__ void emboss_kernel(unsigned char* input, unsigned char* output, int width, int height, int colorWidthStep, int grayWidthStep) {
 
@@ -127,16 +160,13 @@ void readpng_version_info()
       ZLIB_VERSION, zlib_version);
 }
 
-void read_png_file(char* file_name)
-{
+void read_png_file(char* file_name){
         char header[8];
 
-        /* open file and test for it being a png */
         FILE *fp = fopen(file_name, "rb");
         if (!fp)
                 abort_("[read_png_file] File %s could not be opened for reading", file_name);
         fread(header, 1, 8, fp);
-        
 
         /* initialize stuff */
         png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
@@ -174,7 +204,7 @@ void read_png_file(char* file_name)
                 row_pointers[y] = (png_byte*) malloc(png_get_rowbytes(png_ptr,info_ptr));
 
         png_read_image(png_ptr, row_pointers);
-        std::cout << "FIle processed.";
+        std::cout << "File read.";
 
         fclose(fp);
 }
@@ -213,7 +243,7 @@ void process_file(void)
          // Launch the color conversion kernel
         if(flag ==true){
             grayscale_kernel<<<grid, block>>>(d_output, width, height, row_pointers);
-            //addVectors<<<grid, block>>>(dev_a, dev_b, dev_c, size);
+            std::cout << "File converted.";
             }
         status = cudaGetLastError();
         if (status != cudaSuccess)
@@ -242,46 +272,6 @@ int main(int argc, char **argv)
     readpng_version_info();
     read_png_file(argv[1]);
     process_file();
+    write_png_file(argv[2]);
     return 0;
 }
-
-
-//}
-    
-/* void convert(const cv::Mat& input, cv::Mat& output,bool flag) {
-    // Calculate total number of bytes of input and output image
-    const int colorBytes = input.step * input.rows;
-    const int grayBytes = output.step * output.rows;
-    
-    unsigned int* d_kernel;
-
-    unsigned char* d_input, * d_output;
-
-    // Allocate device memory
-    SAFE_CALL(cudaMalloc<unsigned char>(&d_input, colorBytes), "CUDA Malloc Failed");
-    SAFE_CALL(cudaMalloc<unsigned char>(&d_output, grayBytes), "CUDA Malloc Failed");    
-    // Copy data from OpenCV input image to device memory
-    SAFE_CALL(cudaMemcpy(d_input, input.ptr(), colorBytes, cudaMemcpyHostToDevice), "CUDA Memcpy Host To Device Failed");
-
-    // Threads per Block
-    const dim3 block(32, 32);
-
-    // Calculate grid size to cover the whole image
-    const dim3 grid((input.cols + block.x - 1) / block.x, (input.rows + block.y - 1) / block.y);
-
-    // Launch the color conversion kernel
-    if(flag ==true){
-    grayscale_kernel << <grid, block >> > (d_input, d_output, input.cols, input.rows, input.step, output.step);
-    }else {    
-    emboss_kernel << <grid, block >> > (d_input, d_output, input.cols, input.rows, input.step, output.step);
-    }
-    // Synchronize to check for any kernel launch errors
-    SAFE_CALL(cudaDeviceSynchronize(), "Kernel Launch Failed");
-
-    // Copy back data from destination device meory to OpenCV output image
-    SAFE_CALL(cudaMemcpy(output.ptr(), d_output, grayBytes, cudaMemcpyDeviceToHost), "CUDA Memcpy Host To Device Failed");
-
-    // Free the device memory
-    SAFE_CALL(cudaFree(d_input), "CUDA Free Failed");
-    SAFE_CALL(cudaFree(d_output), "CUDA Free Failed");
-}  */
