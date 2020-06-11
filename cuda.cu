44c37e9ed103cf378c66329ce97ab3f2c736791f
Ilona Eisenbraun <ilonaeisenbraun@gmail.com>
2020-06-11 15:16:05 +0200

    temp

--- cuda.cu
+++ cuda.cu
@@ -35,7 +35,7 @@ png_bytep * row_pointers;
 
 
 
-__global__ void grayscale_kernel(unsigned char* output, int width, int height, png_bytep * row_pointers) {
+__global__ void grayscale_kernel(unsigned char* output, int width, int height, png_bytep &row_pointers) {
 
         printf("hello from gpu");
         const int x = blockIdx.x * blockDim.x + threadIdx.x;
