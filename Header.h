1d70242f012beb3d6106d575efbdafb8462f4f3b
UserwithnoUsername <44669677+UserwithnoUsername@users.noreply.github.com>
2020-07-06 22:21:38 +0200

    First master Commit
    >.> fin :)

---
+++ Header.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+#include <chrono>
+#include <fstream>
+#include "mpi.h"
+#include<cuda.h>
+#ifdef __CUDACC__
+#define CUDA_CALLABLE_MEMBER __host__ __device__
+#else
+#define CUDA_CALLABLE_MEMBER
+#endif 
+
+static inline void _safe_cuda_call(cudaError err, const char* msg, const char* file_name, const int line_number) {
+    if (err != cudaSuccess) {
+        fprintf(stderr, "%s\n\nFile: %s\n\nLine Number: %d\n\nReason: %s\n", msg, file_name, line_number, cudaGetErrorString(err));
+        std::cin.get();
+        exit(EXIT_FAILURE);
+    }
+}
+
+/// Safe call macro.
+#define SAFE_CALL(call,msg) _safe_cuda_call((call),(msg),__FILE__,__LINE__)
+
+void convert(const cv::Mat& input, cv::Mat& output, bool flag);
+//extern void convert_to_gray(const cv::Mat& input, cv::Mat& output) {}
---
+++ cuda.cu
@@ -0,0 +1,170 @@
+#include "Header.h"
+
+//https://github.com/evlasblom/cuda-opencv-examples/blob/master/src/bgrtogray.cu
+
+
+
+__global__ void grayscale_kernel(unsigned char* input, unsigned char* output, int width, int height, int colorWidthStep, int grayWidthStep) {
+    {
+        const int x = blockIdx.x * blockDim.x + threadIdx.x;
+        const int y = blockIdx.y * blockDim.y + threadIdx.y;
+        if ((x < width) && (y < height))
+        {
+            //Loc base Image
+            const int color_tid = y * colorWidthStep + (4 * x);
+
+            //Loc in Grayscale
+            const int gray_tid = y * colorWidthStep + (4 * x);
+
+            const unsigned char blue = input[color_tid];
+            const unsigned char green = input[color_tid + 1];
+            const unsigned char red = input[color_tid + 2];
+            const unsigned char alpha = input[color_tid + 3];
+            const float gray = red * 0.21f + green * 0.72 + blue * 0.07f;
+
+            output[gray_tid] = static_cast<unsigned char>(gray);
+            output[gray_tid+1] = static_cast<unsigned char>(gray);
+            output[gray_tid+2] = static_cast<unsigned char>(gray);
+            output[gray_tid+3] = static_cast<unsigned char>(alpha);
+        }
+    }
+
+}
+
+
+__global__ void emboss_kernel(unsigned char* input, unsigned char* output, int width, int height, int colorWidthStep, int grayWidthStep) {
+
+    bool once=true;
+    const int x = blockIdx.x * blockDim.x + threadIdx.x;
+    const int y = blockIdx.y * blockDim.y + threadIdx.y;
+    const int xminus = blockIdx.x * blockDim.x + (threadIdx.x);
+    const int yminus = blockIdx.y * blockDim.y + (threadIdx.y);
+                                    
+    if ((x < width) && (y < height))
+    {
+        const int color_tid = y * colorWidthStep + (4 * x);
+        int emboss_tid=0;
+        //Loc base Image
+        if ((xminus > 1) && (yminus > 1)) {
+            emboss_tid = (yminus)*colorWidthStep + 4 * (xminus - 1);
+
+        }
+        else {
+            emboss_tid  = color_tid;
+        }
+        
+        
+        const float RGB[3]{ input[color_tid] * 1.0f, input[color_tid + 1] * 1.0f, input[color_tid + 2] * 1.0f };
+
+        const float RGBdiff[3]{ input[emboss_tid] * 1.0f, input[emboss_tid + 1] * 1.0f, input[emboss_tid + 2] * 1.0f };
+            
+        
+        const float diffs[3]{ RGB[0] - RGBdiff[0], RGB[1] - RGBdiff[1], RGB[2] - RGBdiff[2] };
+
+
+        float diff = diffs[0];
+        if (abs(diffs[1])>abs(diff)) { diff = diffs[1]; }
+        if (abs(diffs[2]) > abs(diff)) { diff = diffs[2]; }
+
+        float gray = 128 + diff;
+        if (gray > 255) { gray = 255; }
+        if (gray < 0) { gray = 0; }
+        output[color_tid] = static_cast<unsigned char>(gray);
+        output[color_tid + 1] = static_cast<unsigned char>(gray);
+        output[color_tid + 2] = static_cast<unsigned char>(gray);
+        const unsigned char alpha = input[color_tid + 3];
+        output[color_tid + 3] = static_cast<unsigned char>(alpha);
+    }
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    ////row
+     //const int x = blockIdx.x * blockDim.x + threadIdx.x;
+     ////column
+     //const int y = blockIdx.y * blockDim.y + threadIdx.y;
+     //const int color_tid = y * colorWidthStep + (4 * x);
+     //
+   //if ((x < width) && (y < height))
+    //{
+     //   int c = 0;
+     //   for (int i = -1; i < 2; i++) {
+     //       for (int j = -1; j < 2; j++) {
+     //                         
+     //           kernel[c] = (x + i) * colorWidthStep + ( (x + i));
+     //           //printf("KErnelvalue %d", kernel[c]);
+     //           c++;                
+     //       }
+     //   }
+     //   int filterarray[9] = { -1,0,0,0,0,0,0,0,1 };
+     //   int j=0;
+     //   float calc = 0;
+     //   for (int i=0; i < 36;i++) {
+     //       if (i % 4 == 0) {
+     //           j++;
+     //      }
+     //      calc=calc+ filterarray[j] * input[kernel[i]];
+     //      
+     //      printf("calc %d \n", kernel[c]);
+     //   }
+     //   output[color_tid] = static_cast<unsigned char>(calc);
+     //   output[color_tid + 1] = static_cast<unsigned char>(calc);
+     //   output[color_tid + 2] = static_cast<unsigned char>(calc);
+     //   const unsigned char alpha = input[color_tid + 3];
+     //   output[color_tid + 3] = static_cast<unsigned char>(alpha);
+     //
+    //
+    //}
+
+
+//}
+    
+
+
+
+void convert(const cv::Mat& input, cv::Mat& output,bool flag) {
+    // Calculate total number of bytes of input and output image
+    const int colorBytes = input.step * input.rows;
+    const int grayBytes = output.step * output.rows;
+    
+    unsigned int* d_kernel;
+
+    unsigned char* d_input, * d_output;
+
+    // Allocate device memory
+    SAFE_CALL(cudaMalloc<unsigned char>(&d_input, colorBytes), "CUDA Malloc Failed");
+    SAFE_CALL(cudaMalloc<unsigned char>(&d_output, grayBytes), "CUDA Malloc Failed");    
+    // Copy data from OpenCV input image to device memory
+    SAFE_CALL(cudaMemcpy(d_input, input.ptr(), colorBytes, cudaMemcpyHostToDevice), "CUDA Memcpy Host To Device Failed");
+
+    // Threads per Block
+    const dim3 block(32, 32);
+
+    // Calculate grid size to cover the whole image
+    const dim3 grid((input.cols + block.x - 1) / block.x, (input.rows + block.y - 1) / block.y);
+
+    // Launch the color conversion kernel
+    if(flag ==true){
+    grayscale_kernel << <grid, block >> > (d_input, d_output, input.cols, input.rows, input.step, output.step);
+    }else {    
+    emboss_kernel << <grid, block >> > (d_input, d_output, input.cols, input.rows, input.step, output.step);
+    }
+    // Synchronize to check for any kernel launch errors
+    SAFE_CALL(cudaDeviceSynchronize(), "Kernel Launch Failed");
+
+    // Copy back data from destination device meory to OpenCV output image
+    SAFE_CALL(cudaMemcpy(output.ptr(), d_output, grayBytes, cudaMemcpyDeviceToHost), "CUDA Memcpy Host To Device Failed");
+
+    // Free the device memory
+    SAFE_CALL(cudaFree(d_input), "CUDA Free Failed");
+    SAFE_CALL(cudaFree(d_output), "CUDA Free Failed");
+}
